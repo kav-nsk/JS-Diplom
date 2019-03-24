566d672a02205686b736ab98d91eee5ed29220ec
KAV <kav.develop@yandex.ru>
2019-03-24 11:47:22 +0700

    Реализован класс Vector

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
 'use strict';
+class Vector {
+    constructor(x, y) {
+        if (x == undefined && y == undefined) {
+            x = 0;
+            y = 0;
+        }
+        this.x = x;
+        this.y = y;
+    }
+    plus(obj) {
+        if (!(obj instanceof Vector)) {
+            throw new SyntaxError('Можно прибавлять к вектору только вектор типа Vector');
+        } else {
+            return new Vector(this.x + obj.x, this.y + obj.y);
+        }
+    }
+    times(multi) {
+        return new Vector(this.x * multi, this.y * multi);
+    }
+}
+
+const start = new Vector(30, 50);
+const moveTo = new Vector(5, 10);
+const finish = start.plus(moveTo.times(2));
+
+console.log(`Исходное расположение: ${start.x}:${start.y}`);
+console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
