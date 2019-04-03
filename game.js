4483e73612fee8b42799e769d172a10af0f5f770
KAV <kav.develop@yandex.ru>
2019-04-03 21:26:08 +0700

    недоделанный Level на показ 03.04.19

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 'use strict';
+
 class Vector {
     constructor(x, y) {
         if (x == undefined && y == undefined) {
@@ -77,7 +78,7 @@ class Actor {
         } else if (objMove === this) {
             return false;
         }
-        // игнорирование объектов с отрицательными размерами
+        // Игнорирование объектов с отрицательными размерами.
         if ((objMove.size.x < 0 || objMove.size.y < 0) &&
         ((objMove.left == this.left && objMove.bottom == this.bottom) || (objMove.right == this.right && objMove.top == this.top) ||
         (objMove.left == this.left && objMove.top == this.top) || (objMove.bottom == this.bottom && objMove.right == this.right))) {
@@ -88,18 +89,18 @@ class Actor {
         (objMove.left == this.left && objMove.top == this.top) || (objMove.bottom == this.bottom && objMove.right == this.right))) {
             return false;
         }
-        // расчет длины диагонали объектов
+        // Расчет длины диагонали объектов.
         let diagStatObj = Math.pow(this.size.x, 2) + Math.pow(this.size.y, 2);
         let diagMoveObj = Math.pow(objMove.size.x, 2) + Math.pow(objMove.size.y, 2);
-        // объекты одинаковы по размеру и расположены один над другим
+        // Объекты одинаковы по размеру и расположены один над другим.
         if ((diagStatObj == diagMoveObj) && (this.left == objMove.left)) {
             return true;
         }
-        // статичный больше подвижного и подвижный находится в нем
+        // Статичный больше подвижного и подвижный находится в нем.
         if ((diagStatObj > diagMoveObj) && (objMove.left > this.left && objMove.right < this.right)) {
             return true;
         }
-        // случаи частичного пересечения объектов
+        // Случаи частичного пересечения объектов.
         if (((objMove.bottom >= this.bottom && objMove.bottom < this.top) && (objMove.left >= this.left && objMove.left < this.right)) ||
         ((objMove.bottom >= this.bottom && objMove.bottom < this.top) && (objMove.right <= this.right && objMove.right > this.left)) ||
         ((objMove.top <= this.top && objMove.top > this.bottom) && (objMove.left >= this.left && objMove.left < this.right)) ||
@@ -110,4 +111,91 @@ class Actor {
         }
         return false;
     }
+}
+
+class Level {
+    constructor(grid, listActorsObj) {
+        this.grid = grid;
+        this.actors = listActorsObj;
+        this.status = null;
+        this.finishDelay = 1;
+
+        if (listActorsObj != undefined) {
+            listActorsObj.forEach(i => {
+                if (i.type == 'player') {
+                    this.player = i;
+                }
+            });
+        };
+
+         // Получаем параметры высоты и ширины игрового поля.
+        if (grid != undefined && grid.length != 0) {
+            this.height = grid.length;   
+            this.width = 0;
+            for (let i of grid) {
+                if (i != undefined && i.length > this.width) {
+                    this.width = i.length;
+                };
+            }
+        } else {
+            this.height = 0;
+            this.width = 0;
+        };
+    }
+
+    isFinished() {
+        if (this.status != null && this.finishDelay < 0) {
+            return true;
+        } else {
+            return false;
+        };
+    }
+
+    actorAt(objMove) {
+        if (this.actors == undefined) {
+            return undefined;
+        };
+        let result = undefined;
+        if (objMove == undefined || !(objMove instanceof Actor)) {
+            throw new SyntaxError('Требуется объект типа Actor');
+        } else {
+            this.actors.forEach(i => {
+            if (i.isIntersect(objMove)) {
+                result = i;
+            };
+        });
+        return result;
+    };
+    }
+
+    obstacleAt(pos, objSize) {
+        if (!(pos instanceof Vector) || !(objSize instanceof Vector)) {
+            throw new SyntaxError('Требуется объект типа Vector');
+        } else {
+            let distObj = pos.plus(objSize);
+            // Проверка выхода за боковые границы поля
+            if ((distObj.x < 1 && distObj.y >=1 && distObj.y <= this.height) ||
+            (distObj.x > this.width && distObj.y >=1 && distObj.y <= this.height)) {
+                return 'wall';
+            };
+            // Проверка выхода за верхнюю границу
+            if (distObj.y > this.height) {
+                return 'lava';
+            };
+            // Проверка выхода за нижнюю границу
+            if (distObj.y < 1) {
+                return 'wall';
+            };
+            // Проверка пересечений стен и лав:
+            if (this.grid[distObj.x][distObj.y] == 'wall') {
+                return 'wall';
+            };
+            if (this.grid[distObj.x][distObj.y] == 'lava') {
+                return 'lava';
+            };
+                // - пересечение со стеной объекта с не целочисленными координатами.
+
+        };
+    }
+
 }
